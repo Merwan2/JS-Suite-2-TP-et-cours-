5bd717f526bc4af9aa9873513edbcca32343884e
Merwan Barkallah <merwan.barkallah@gmail.com>
2022-01-21 11:30:15 +0100

    Ajout TP1 Js

---
+++ Cours JS/TP1 JS.js
@@ -0,0 +1,10 @@
+
+const number1 = parseFloat(prompt("Entrer nombre 1 "));
+const number2 = parseFloat(prompt("Entrer nombre 2 "));
+
+let result; 
+result = number1 + number2;
+alert('Bienvenue sur la page de formulaire')
+
+console.log(number1 + " et " + number2); 
+console.log("Le resultat est " + result)
